022fe0b960fc9ca6e15eb29cb92c2a01eb30155b
frank <frank@qlimix.net>
2018-02-18 13:35:32 +0100

    environment inital

---
+++ src/Environment.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+final class Environment
+{
+    private const PRODUCTION = 'prod';
+    private const DEVELOPMENT = 'dev';
+
+    /** @var string */
+    private $env;
+
+    /**
+     * @param string $env
+     */
+    public function __construct(string $env)
+    {
+        $this->env = $env;
+    }
+
+    /**
+     * @return Environment
+     */
+    public function createDevelopment(): Environment
+    {
+        return new self(self::DEVELOPMENT);
+    }
+
+    /**
+     * @return Environment
+     */
+    public function createProduction(): Environment
+    {
+        return new self(self::PRODUCTION);
+    }
+
+    /**
+     * @param Environment $environment
+     *
+     * @return bool
+     */
+    public function equals(Environment $environment): bool
+    {
+        return $environment->getEnv() === $this->env;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEnv(): string
+    {
+        return $this->env;
+    }
+}
